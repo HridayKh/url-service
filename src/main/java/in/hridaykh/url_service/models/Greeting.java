f15f9a8f20232d470ffbfe13e220f0a74dc05392
HridayKh <hridaykh1234@gmail.com>
2026-02-04 17:28:51 +0530

    first commit

---
+++ src/main/java/in/hridaykh/url_service/models/Greeting.java
@@ -0,0 +1,4 @@
+package in.hridaykh.url_service.models;
+
+public record Greeting(long id, String content) {
+}
---
+++ src/test/java/in/hridaykh/url_service/UrlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.hridaykh.url_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
